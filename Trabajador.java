36f158438ae1cf10da29adb8290e21d92561f8d1
mh0316 <85312998+mh0316@users.noreply.github.com>
2022-06-19 15:46:58 -0400

    Update Trabajador.java

--- Trabajador.java
+++ Trabajador.java
@@ -4,30 +4,18 @@ public class Trabajador extends Persona implements IArchivo {
 	private String afp;
 
 	public Srtring getIsapre() {
-		// TODO - implement Trabajador.getIsapre
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param isapre
-	 */
 	public void setIsapre(string isapre) {
-		// TODO - implement Trabajador.setIsapre
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param afp
-	 */
 	public void setAfp(string afp) {
-		// TODO - implement Trabajador.setAfp
 		throw new UnsupportedOperationException();
 	}
 
 	public Trabajador() {
-		// TODO - implement Trabajador.Trabajador
 		throw new UnsupportedOperationException();
 	}
 
